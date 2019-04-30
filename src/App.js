2652e1395cb064d87ebdf4488e8758993d7d4b34
Slawomir Dyk <slawomirdyk@gmail.com>
2019-04-30 12:30:02 +0100

    Class App in App.js file sorted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
+class App extends Component {
+  render () {
+    return (
     <div className="App">
       <h1>Hi, I am a react app</h1>
       <p>My name is Slawek</p>
       <Person />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
