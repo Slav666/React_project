ac4cd9f82719be6da08a8b864ed056e247b413be
Slawomir Dyk <slawomirdyk@gmail.com>
2019-04-30 12:20:42 +0100

    Arrow funcion create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I am a react app</h1>
       <p>My name is Slawek</p>
+      <Person />
     </div>
   );
 }
