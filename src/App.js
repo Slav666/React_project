7a7bf55dc081e571a7fa9f956677b2b77609767f
Slawomir Dyk <slawomirdyk@gmail.com>
2019-04-30 14:26:37 +0100

    Add state and handling event with method

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "John", age: 29},
+      {name: "Alex" ,age:37},
+      {name: "Chris", age: 35}
+      
+    ]
+  }
+  swichNameHandler = () => {
+    console.log('Was Cliced');
+  }
   render () {
     return (
     <div className="App">
       <h1>Hi, I am a react app</h1>
       <p>My name is Slawek</p>
-      <Person name  = "John" age = "29" />
-      <Person name = "Alex" age= "37">My hobies: coding</Person>
-      <Person name = "Chris" age ="35"/>
+      <button onClick={this.swichNameHandler}>Swich Name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobies: coding</Person>
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
     </div>
     );
   }
